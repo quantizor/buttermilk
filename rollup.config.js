86474cbe16929d86db706580ddb8dd09f412da10
Evan Scott <probablyup@gmail.com>
2018-03-03 14:10:20 -0500

    add utils export

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps'
 
-import pkg from './package.json';
-
 const plugins = [
     babel({
         externalHelpers: true,
