2b1a19b72b87086dd0eddd8b4cdc0de2b73c9573
Evan Scott <probablyup@gmail.com>
2018-06-18 16:24:28 -0400

    seo tweaks for the website

--- site/pages/_document.js
+++ site/pages/_document.js
@@ -20,22 +20,56 @@ export default class MyDocument extends Document {
             <html>
                 <Head>
                     <meta charSet="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
                     <meta name="theme-color" content="#8a0000" />
-                    <meta name="description" content="Beautifully simple routing for React JavaScript projects. Lightweight and works well both on the browser and server." />
-                    <meta name="og:title" content="Buttermilk, a React routing library" />
-                    <meta name="og:description" content="Lightweight, simple, and works well both on the browser and server." />
-                    <meta name="twitter:image" content="https://buttermilk.js.org/favicon_retina.png" />
-                    <meta property="og:image" content="https://buttermilk.js.org/favicon_retina.png" />
+                    <meta
+                        name="description"
+                        content="The lightweight, dead-simple, hyper customizable alternative to React Router. Designed for both client and server-side routing."
+                    />
+                    <meta
+                        name="keywords"
+                        content="react router, javascript, client side router, server side router, routing library"
+                    />
+                    <meta
+                        name="og:title"
+                        content="Buttermilk, a React routing library"
+                    />
+                    <meta
+                        name="og:description"
+                        content="Lightweight, simple, and works well both on the browser and server."
+                    />
+                    <meta
+                        name="twitter:image"
+                        content="https://buttermilk.js.org/favicon_retina.png"
+                    />
+                    <meta
+                        property="og:image"
+                        content="https://buttermilk.js.org/favicon_retina.png"
+                    />
                     <meta name="twitter:card" content="summary" />
                     <meta name="twitter:creator" content="@probablyup" />
                     <link rel="shortcut icon" href="/static/favicon.png" />
-                    <title>Buttermilk</title>
+                    <title>
+                        Buttermilk | Beautifully simple routing for React
+                        projects
+                    </title>
 
-                    <link href="https://fonts.googleapis.com/css?family=Karma:400,600,700|Overpass+Mono|Vibur" rel="stylesheet" />
-                    <link href="/static/vendor/solarized-light.css" rel="stylesheet" />
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Karma:400,600,700|Overpass+Mono|Vibur"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="/static/vendor/solarized-light.css"
+                        rel="stylesheet"
+                    />
 
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-91833843-2"></script>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-91833843-2"
+                    />
                     <script dangerouslySetInnerHTML={{ __html: GA }} />
                     {this.props.styleTags}
                 </Head>
@@ -44,7 +78,7 @@ export default class MyDocument extends Document {
                     <Main />
                     <NextScript />
 
-                    <script src="/static/vendor/rainbow.min.js"></script>
+                    <script src="/static/vendor/rainbow.min.js" />
                     <script dangerouslySetInnerHTML={{ __html: Rainbow }} />
                 </body>
             </html>
