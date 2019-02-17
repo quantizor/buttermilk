90bf664b6b36a8f77b81398559c02403c8e70356
Evan Jacobs <probablyup@gmail.com>
2019-02-17 15:01:00 -0600

    add react-is to rollup settings, remove unused babel transform

--- rollup.config.js
+++ rollup.config.js
@@ -7,77 +7,100 @@ import sourceMaps from 'rollup-plugin-sourcemaps';
 import uglify from 'rollup-plugin-uglify';
 
 function generateConfig(overrides, minify = false, externalHelpers = false) {
-    return merge({}, {
-        input: 'src/index.js',
-        output: {
-            globals: { react: 'React', 'prop-types': 'PropTypes' },
-            name: 'Buttermilk',
-            sourcemap: true,
-        },
-        external: ['react', 'prop-types'],
-        plugins: [
-            resolve(),
-            babel({
-                babelrc: false,
-                plugins: [
-                    'transform-class-properties',
-                ],
-                runtimeHelpers: externalHelpers,
-                presets: [
-                    ['env', {
-                        modules: false,
-                    }],
-                    'react'
-                ],
-            }),
-            commonjs({
-                ignoreGlobal: true,
-            }),
-            sourceMaps(),
-            replace({
-                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            }),
-            minify ? uglify() : null,
-        ].filter(Boolean),
-    }, overrides);
+  return merge(
+    {},
+    {
+      input: 'src/index.js',
+      output: {
+        globals: { react: 'React', 'react-is': 'React', 'prop-types': 'PropTypes' },
+        name: 'Buttermilk',
+        sourcemap: true,
+      },
+      external: ['react', 'react-is', 'prop-types'],
+      plugins: [
+        resolve(),
+        babel({
+          babelrc: false,
+          runtimeHelpers: externalHelpers,
+          presets: [
+            [
+              'env',
+              {
+                modules: false,
+              },
+            ],
+            'react',
+          ],
+        }),
+        commonjs({
+          ignoreGlobal: true,
+        }),
+        sourceMaps(),
+        replace({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
+        minify ? uglify() : null,
+      ].filter(Boolean),
+    },
+    overrides
+  );
 }
 
 export default [
-    generateConfig({
-        output: {
-            file: 'dist/standalone.js',
-            format: 'iife',
-        }
-    }),
-    generateConfig({
-        output: {
-            file: 'dist/standalone.min.js',
-            format: 'iife',
-        }
-    }, true),
-    generateConfig({
-        output: {
-            file: 'dist/cjs.js',
-            format: 'cjs',
-        }
-    }, false, true),
-    generateConfig({
-        output: {
-            file: 'dist/cjs.min.js',
-            format: 'cjs',
-        }
-    }, true, true),
-    generateConfig({
-        output: {
-            file: 'dist/es.js',
-            format: 'es',
-        }
-    }, false, true),
+  generateConfig({
+    output: {
+      file: 'dist/standalone.js',
+      format: 'iife',
+    },
+  }),
+  generateConfig(
+    {
+      output: {
+        file: 'dist/standalone.min.js',
+        format: 'iife',
+      },
+    },
+    true
+  ),
+  generateConfig(
+    {
+      output: {
+        file: 'dist/cjs.js',
+        format: 'cjs',
+      },
+    },
+    false,
+    true
+  ),
+  generateConfig(
+    {
+      output: {
+        file: 'dist/cjs.min.js',
+        format: 'cjs',
+      },
+    },
+    true,
+    true
+  ),
+  generateConfig(
+    {
+      output: {
+        file: 'dist/es.js',
+        format: 'es',
+      },
+    },
+    false,
+    true
+  ),
 
-    generateConfig({
-        output: {
-            file: 'dist/es.min.js',
-            format: 'es',
-        }
-    }, true, true),
+  generateConfig(
+    {
+      output: {
+        file: 'dist/es.min.js',
+        format: 'es',
+      },
+    },
+    true,
+    true
+  ),
 ];
