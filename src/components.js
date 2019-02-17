89cb7cec4bf613ab5292c6184383cfe55f2d749e
Evan Jacobs <probablyup@gmail.com>
2019-02-16 20:03:12 -0600

    wip

--- src/components.js
+++ src/components.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 import { createRouteContext, findRoute, processRoute, route, valid } from './utils';
 
-const { Provider, Consumer } = React.createContext();
+const ButtermilkContext = React.createContext();
 const BROWSER = typeof window !== 'undefined';
 const NOOP = function() {};
+let noFallbackWarningEmitted = false;
 
 /**
  * The gist of Buttermilk's router is that it acts like a controlled component when used
@@ -16,221 +16,166 @@ const NOOP = function() {};
  * The router will also automatically pick up popstate events caused by user-driven browser
  * navigation (forward, back buttons, etc.)
  */
-export class Router extends React.Component {
-  static propTypes = {
-    /**
-     * Provide a spinner or something to look at while the promise
-     * is in flight if using async routes.
-     */
-    loadingComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+export function Router(props) {
+  if (!BROWSER && !props.url) {
+    throw new Error('props.url is required for non-browser environments');
+  }
 
-    /**
-     * An optional app runtime component. Think of it like the "shell" of your
-     * app, so perhaps the outer container, nav bar, etc. You'll probably want to
-     * put any "Provider" type components here that are intended to wrap your
-     * whole application.
-     */
-    outerComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  const url = props.url || window.location.href;
 
-    routes: PropTypes.arrayOf(
-      PropTypes.shape({
-        /**
-         * A RegExp, string, or function accepting the URL as
-         * an argument and returning a boolean if valid.
-         */
-        path: PropTypes.oneOfType([PropTypes.instanceOf(RegExp), PropTypes.string, PropTypes.func]).isRequired,
+  const [routes, updateRoutes] = useState(processRoutes(props.routes));
+  const [routingState, updateRoutingState] = useState(getStateUpdateForUrl(routes, url));
+  const contextValue = createRouteContext(routingState.activeRoute, routingState.url);
 
-        /**
-         * A string URL path to a different route. If this is given,
-         * then "render" is not required.
-         */
-        redirect: PropTypes.string,
+  useEffect(() => {
+    props.routerDidInitialize(contextValue);
 
-        /**
-         * A function that returns one of the following:
-         *
-         * 1. JSX.
-         * 2. A React component class.
-         * 3. A promise resolving to JSX or a React component class.
-         */
-        render: PropTypes.func,
-      })
-    ).isRequired,
+    function handleLocationChange() {
+      const { route: nextRoute, url: nextUrl } = findRoute(routes, window.location.href);
+      const nextValue = createRouteContext(nextRoute, nextUrl);
+      const result = props.routeWillChange(contextValue, nextValue);
+      const cb = () => props.routeDidChange(nextValue, contextValue);
+      const finish = () => updateRoutingState(getStateUpdateForUrl(routes, nextUrl), cb);
 
-    /**
-     * A hook for reacting to an impending route transition. Accepts a promise
-     * and will pause the route transition until the promise is resolved. Return
-     * false or reject a given promise to abort the routing update.
-     *
-     * Provides currentRouting and nextRouting as arguments.
-     */
-    routeWillChange: PropTypes.func,
-
-    /**
-     * A hook for reacting to a completed route transition. It might be used
-     * for synchronizing some global state if desired.
-     *
-     * Provides currentRouting and previousRouting as arguments.
-     */
-    routeDidChange: PropTypes.func,
-
-    /**
-     * A hook for synchronizing initial routing state.
-     *
-     * Providers initialRouting as an argument.
-     */
-    routerDidInitialize: PropTypes.func,
-
-    /**
-     * The initial URL to be used for processing, falls back to
-     * window.location.href for non-SSR. Required for environments without
-     * browser navigation eventing.
-     */
-    url: PropTypes.string,
-  };
-
-  static defaultProps = {
-    loadingComponent: 'div',
-    outerComponent: 'div',
-    routeDidChange: NOOP,
-    routeWillChange: NOOP,
-    routerDidInitialize: NOOP,
-    url: '',
-  };
-
-  static getURL = function() {
-    return window.location.href;
-  };
-
-  noFallbackWarningEmitted = false;
-  promise = null;
-
-  constructor(props, context) {
-    super(props, context);
-
-    if (!BROWSER && !props.url) {
-      throw new Error('props.url is required for non-browser environments');
+      if (result === false) return;
+      else if (result instanceof Promise) result.then(finish, NOOP);
+      else finish();
     }
 
-    const routes = this.processRoutes(props.routes);
-    const url = props.url || Router.getURL();
+    window.addEventListener('popstate', handleLocationChange);
+    window.addEventListener('hashchange', handleLocationChange);
 
-    this.state = this.getStateUpdateForUrl(url, routes);
+    return () => {
+      window.removeEventListener('popstate', handleLocationChange);
+      window.removeEventListener('hashchange', handleLocationChange);
+    };
+  }, []);
 
-    props.routerDidInitialize(createRouteContext(this.state.activeRoute, url));
-  }
+  useEffect(() => updateRoutes(processRoutes(props.routes)), [url]);
+  useEffect(() => updateRoutingState(getStateUpdateForUrl(routes, url)), [props.routes, props.url]);
+
+  const Renderable = routingState.children;
+
+  return (
+    <ButtermilkContext.Provider value={contextValue}>
+      <props.outerComponent {...contextValue}>
+        <Suspense fallback={props.loadingComponent}>
+          {!React.isValidElement(Renderable) ? <Renderable {...contextValue} /> : Renderable}
+        </Suspense>
+      </props.outerComponent>
+    </ButtermilkContext.Provider>
+  );
+}
+
+Router.propTypes = {
+  /**
+   * Provide a spinner or something to look at while the promise
+   * is in flight if using async routes.
+   */
+  loadingComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 
   /**
-   * In a browser setting, we only want to rely on browser navigation events
-   * to determine routing updates.
+   * An optional app runtime component. Think of it like the "shell" of your
+   * app, so perhaps the outer container, nav bar, etc. You'll probably want to
+   * put any "Provider" type components here that are intended to wrap your
+   * whole application.
    */
-  shouldRecompute(nextProps) {
-    return nextProps.routes !== this.props.routes || (nextProps.url !== this.state.url && !BROWSER);
+  outerComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  routes: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * A RegExp, string, or function accepting the URL as
+       * an argument and returning a boolean if valid.
+       */
+      path: PropTypes.oneOfType([PropTypes.instanceOf(RegExp), PropTypes.string, PropTypes.func]).isRequired,
+
+      /**
+       * A string URL path to a different route. If this is given,
+       * then "render" is not required.
+       */
+      redirect: PropTypes.string,
+
+      /**
+       * A function that returns one of the following:
+       *
+       * 1. JSX.
+       * 2. A React component class.
+       * 3. A promise resolving to JSX or a React component class.
+       */
+      render: PropTypes.func,
+    })
+  ).isRequired,
+
+  /**
+   * A hook for reacting to an impending route transition. Accepts a promise
+   * and will pause the route transition until the promise is resolved. Return
+   * false or reject a given promise to abort the routing update.
+   *
+   * Provides currentRouting and nextRouting as arguments.
+   */
+  routeWillChange: PropTypes.func,
+
+  /**
+   * A hook for reacting to a completed route transition. It might be used
+   * for synchronizing some global state if desired.
+   *
+   * Provides currentRouting and previousRouting as arguments.
+   */
+  routeDidChange: PropTypes.func,
+
+  /**
+   * A hook for synchronizing initial routing state.
+   *
+   * Providers initialRouting as an argument.
+   */
+  routerDidInitialize: PropTypes.func,
+
+  /**
+   * The initial URL to be used for processing, falls back to
+   * window.location.href for non-SSR. Required for environments without
+   * browser navigation eventing.
+   */
+  url: PropTypes.string,
+};
+
+Router.defaultProps = {
+  loadingComponent: 'div',
+  outerComponent: 'div',
+  routeDidChange: NOOP,
+  routeWillChange: NOOP,
+  routerDidInitialize: NOOP,
+  url: '',
+};
+
+function processChildren(unknown) {
+  if (unknown instanceof Promise) {
+    return React.lazy(unknown);
+  } else {
+    return unknown;
   }
+}
 
-  componentWillReceiveProps(nextProps) {
-    if (this.shouldRecompute(nextProps)) {
-      this.recomputeRoutingState(BROWSER ? this.state.url : nextProps.url, nextProps.routes);
-    }
-  }
+function getStateUpdateForUrl(routes, url) {
+  const result = findRoute(routes, url);
 
-  componentDidMount() {
-    window.addEventListener('popstate', this.handleLocationChange);
-    window.addEventListener('hashchange', this.handleLocationChange);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('popstate', this.handleLocationChange);
-    window.removeEventListener('hashchange', this.handleLocationChange);
-  }
-
-  render() {
-    const contextValue = createRouteContext(this.state.activeRoute, this.state.url);
-
-    return (
-      <Provider value={contextValue}>
-        <this.props.outerComponent {...contextValue}>
-          {this.renderChildren(this.state.children, contextValue)}
-        </this.props.outerComponent>
-      </Provider>
-    );
-  }
-
-  renderChildren(renderable, routingProps) {
-    if (renderable === null) {
-      return React.createElement(this.props.loadingComponent, routingProps);
-    } else if (!React.isValidElement(renderable)) {
-      return React.createElement(renderable, routingProps);
-    } else {
-      return renderable;
-    }
-  }
-
-  processChildren(unknown) {
-    if (unknown instanceof Promise) {
-      const instance = this;
-      instance.promise = unknown;
-      unknown.then(
-        function handlePromiseResolution(result) {
-          /**
-           * Is this promise still valid? If not, ignore the
-           * resolution.
-           */
-          if (instance.promise === this) {
-            instance.setState({ children: result });
-            this.promise = null;
-          }
-        }.bind(unknown),
-        NOOP
-      );
-
-      return null;
-    } else {
-      return unknown;
-    }
-  }
-
-  handleLocationChange = (/* event */) => {
-    const currentValue = createRouteContext(this.state.activeRoute, this.state.url);
-    const { route: nextRoute, url: nextUrl } = findRoute(this.state.routes, Router.getURL());
-    const nextValue = createRouteContext(nextRoute, nextUrl);
-    const result = this.props.routeWillChange(currentValue, nextValue);
-    const cb = () => this.props.routeDidChange(nextValue, currentValue);
-    const finish = () => this.recomputeRoutingState(nextUrl, this.state.routes, cb);
-
-    if (result === false) return;
-    else if (result instanceof Promise) result.then(finish, NOOP);
-    else finish();
+  return {
+    activeRoute: result.route,
+    children: processChildren(result.route.render(createRouteContext(result.route, result.url))),
+    routes,
+    url: result.url,
   };
+}
 
-  recomputeRoutingState = (url, routes, cb) => {
-    this.setState(
-      this.getStateUpdateForUrl(url, routes === this.state.routes ? routes : this.processRoutes(routes)),
-      cb
-    );
-  };
-
-  processRoutes(routes) {
-    if (process.env.NODE_ENV !== 'production') {
-      if (!this.noFallbackWarningEmitted && routes.every(route => route.path !== '*')) {
-        console.warn('no fallback route "*" was supplied. if a matching route is not found, the router will throw');
-        this.noFallbackWarning = true;
-      }
+function processRoutes(routes) {
+  if (process.env.NODE_ENV !== 'production') {
+    if (!noFallbackWarningEmitted && routes.every(route => route.path !== '*')) {
+      console.warn('no fallback route "*" was supplied. if a matching route is not found, the router will throw');
+      noFallbackWarning = true;
     }
-
-    return routes.map(processRoute);
   }
 
-  getStateUpdateForUrl(url, routes) {
-    const result = findRoute(routes, url);
-
-    return {
-      activeRoute: result.route,
-      children: this.processChildren(result.route.render(createRouteContext(result.route, result.url))),
-      routes,
-      url: result.url,
-    };
-  }
+  return routes.map(processRoute);
 }
 
 /**
@@ -242,7 +187,7 @@ export class Router extends React.Component {
  *   }}
  * </RoutingState>
  */
-export const RoutingState = Consumer;
+export const RoutingState = ButtermilkContext.Consumer;
 
 /**
  * A polymorphic anchor link component. On click/tap/enter if the destination
@@ -254,73 +199,64 @@ export const RoutingState = Consumer;
  *
  * Adds [data-active] if the given href matches the active route.
  */
-export class Link extends React.PureComponent {
-  static propTypes = {
-    /**
-     * An HTML tag name or valid ReactComponent class to be rendered. Must
-     * be compatible with React.createElement.
-     *
-     * Defaults to an anchor "a" tag.
-     */
-    as: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+export const Link = React.forwardRef(function ButtermilkLink(props, ref) {
+  const routingState = useContext(ButtermilkContext);
 
-    /**
-     * A valid relative or absolute URL string.
-     */
-    href: PropTypes.string.isRequired,
+  return React.createElement(
+    props.as || 'a',
+    Object.assign({}, props, {
+      'data-active': valid(routingState.route.test, props.href) ? '' : undefined,
+      'data-href': props.href,
+      as: undefined,
+      href: shouldRenderAnchorProps(props) ? props.href : undefined,
+      ref,
+      role: shouldRenderRole(props) ? 'link' : undefined,
+      onClick: handleNavigationIntent,
+      tabIndex: props.tabIndex || 0,
+      target: shouldRenderAnchorProps(props) ? props.target : undefined,
+    })
+  );
+});
 
-    /**
-     * Any valid value of the anchor tag "target" attribute.
-     *
-     * See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attr-target
-     */
-    target: PropTypes.string,
-  };
+Link.propTypes = {
+  /**
+   * An HTML tag name or valid ReactComponent class to be rendered. Must
+   * be compatible with React.createElement.
+   *
+   * Defaults to an anchor "a" tag.
+   */
+  as: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 
-  static defaultProps = {
-    as: 'a',
-  };
+  /**
+   * A valid relative or absolute URL string.
+   */
+  href: PropTypes.string.isRequired,
 
-  render() {
-    return (
-      <Consumer>
-        {({ route }) => {
-          return React.createElement(
-            this.props.as,
-            Object.assign({}, this.props, {
-              'data-active': valid(route.test, this.props.href) ? '' : undefined,
-              as: undefined,
-              href: this.shouldRenderAnchorProps() ? this.props.href : undefined,
-              role: this.shouldRenderRole() ? 'link' : undefined,
-              onClick: this.handleNavigationIntent,
-              onKeyDown: this.handleNavigationIntent,
-              onTouchEnd: this.handleNavigationIntent,
-              target: this.shouldRenderAnchorProps() ? this.props.target : undefined,
-            })
-          );
-        }}
-      </Consumer>
-    );
+  /**
+   * Any valid value of the anchor tag "target" attribute.
+   *
+   * See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attr-target
+   */
+  target: PropTypes.string,
+};
+
+function shouldRenderAnchorProps(props) {
+  return props.as === 'a' || typeof props.as !== 'string';
+}
+
+function shouldRenderRole(props) {
+  return props.as !== 'a';
+}
+
+function handleNavigationIntent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  const href = event.target.getAttribute('data-href');
+
+  if (event.metaKey || event.target.getAttribute('target') === '_blank') {
+    window.open(href);
+  } else {
+    route(href);
   }
-
-  shouldRenderAnchorProps() {
-    return this.props.as === Link.defaultProps.as || typeof this.props.as !== 'string';
-  }
-
-  shouldRenderRole() {
-    return this.props.as !== Link.defaultProps.as;
-  }
-
-  handleNavigationIntent = e => {
-    if (e.type !== 'keydown' || (e.type === 'keydown' && (e.key === 'Enter' || e.key === 'Space'))) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      if (e.metaKey || e.target.getAttribute('target') === '_blank') {
-        window.open(this.props.href);
-      } else {
-        route(this.props.href);
-      }
-    }
-  };
 }
